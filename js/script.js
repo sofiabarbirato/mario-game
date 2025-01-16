0ab656c9be3cd469cb3c82b52d882d3dec3e3e45
sofiabarbirato <sofiabarbirato@gmail.com>
2025-01-16 16:27:01 -0300

    feat: adiciona os arquivos ao repositorio

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const gameOver = document.querySelector('.game-over');
+const botao = document.querySelector('.button-mario');
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500)
+}
+
+const loop =  setInterval(() => {
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replaceAll('px', '');
+
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+    pipe.style.animation = 'none';  
+    pipe.style.left = `${pipePosition}px`; 
+
+    mario.style.animation = 'none';  
+    pipe.style.left = `${marioPosition}px`; 
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '100px';
+    mario.style.marginLeft = '50px';
+
+    gameOver.src = './images/texto-game-over.png';
+    gameOver.style.display = 'flex';
+
+    botao.style.display = 'flex';
+
+
+    clearInterval(loop);
+    }
+
+}, 10);
+
+
+document.addEventListener('keydown', jump);  
